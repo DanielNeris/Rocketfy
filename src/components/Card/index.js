5870a58588f562b308568d9847df3a1dbab89c18
Daniel de Souza Neri <danielneris01@gmail.com>
2019-08-18 18:48:17 -0300

    moving from list to anothelist and ao contrario

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -20,12 +20,12 @@ export default function Card({ data, index, listIndex }) {
         accept: 'CARD',
         hover(item, monitor) {
             const draggedListIndex = item.listIndex;
-            //const targetListIndex = listIndex;
+            const targetListIndex = listIndex;
 
             const draggedIndex = item.index;
             const targetIndex = index;
 
-            if (draggedIndex === targetIndex) return;
+            if (draggedIndex === targetIndex && draggedListIndex === targetListIndex) return;
 
             const targetSize = ref.current.getBoundingClientRect();
             const targetCenter = (targetSize.bottom - targetSize.top) / 2;
@@ -37,9 +37,10 @@ export default function Card({ data, index, listIndex }) {
 
             if (draggedIndex > targetIndex && draggedTop > targetCenter) return;
 
-            move(draggedListIndex, draggedIndex, targetIndex);
+            move(draggedListIndex, targetListIndex, draggedIndex, targetIndex);
 
             item.index = targetIndex;
+            item.listIndex = targetListIndex;
         }
     });
 
